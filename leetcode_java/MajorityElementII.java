aa4457c71db8403b68387d0f4470ad6e4b471e92
tudejian <tudejian@gmail.com>
2015-07-11 03:26:16 -0700

    leetcode_java/MajorityElementII.java

--- leetcode_java/MajorityElementII.java
+++ leetcode_java/MajorityElementII.java
@@ -6,6 +6,50 @@
 
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        int n1 = 0, n2 = 0;
+        int c1 = 0, c2 = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; ++i) {
+            if (n1 == nums[i]) {
+                ++c1;
+            } else if (n2 == nums[i]) {
+                ++c2;
+            } else if (c1 == 0) {
+                n1 = nums[i];
+                c1 = 1;
+            } else if (c2 == 0) {
+                n2 = nums[i];
+                c2 = 1;
+            } else {
+                --c1;
+                --c2;
+            }
+        }
+
+        c1 = c2 = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == n1) {
+                ++c1;
+            } else if (nums[i] == n2) {
+                ++c2;
+            }
+        }
+        if (c1 > n / 3) result.add(n1);
+        if (c2 > n / 3) result.add(n2);
+        return result;
+    }
+}
+
+// generic algorithm for k majority elment (occurs more than int(n/k) times)  problem.
+// Complexity: NlogK
+// Space: K
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
         return majorityElement(nums, 3);
     }
 
