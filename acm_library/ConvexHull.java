e326267be75845f598029342a733170c42055787
tudejian <tudejian@gmail.com>
2017-05-14 11:30:46 -0700

    Update ConvexHull.java

--- acm_library/ConvexHull.java
+++ acm_library/ConvexHull.java
@@ -5,11 +5,19 @@ public class ConvexHull {
 		return Long.signum(((long) a.x - o.x) * (b.y - o.y) - ((long) a.y - o.y) * (b.x - o.x));
 	}
 
-	public List<Point> convexHull(Point[] points, boolean inclusive/* includes inline points */) {
+	public Point[] convexHull(Point[] points, boolean inclusive) {
+		int n = points.length, signum = (inclusive ? 1 : 0);
+		if (n <= 2) {
+			if (n == 2 && !inclusive && points[0].x == points[1].x && points[0].y == points[1].y) {
+				return Arrays.copyOf(points, 1);
+			} else {
+				return Arrays.copyOf(points, n);
+			}
+		}
 		Arrays.sort(points, (a, b) -> {
 			return (a.x == b.x) ? Integer.compare(a.y, b.y) : Integer.compare(a.x, b.x);
 		});
-		int n = points.length, signum = (inclusive ? 1 : 0), m1 = 0, m2 = 0;
+		int m1 = 0, m2 = 0;
 		Point[] q1 = new Point[n], q2 = new Point[n];
 		for (int i = 0; i < n; i++) {
 			while (m1 >= 2 && cross(q1[m1 - 2], q1[m1 - 1], points[i]) >= signum)
@@ -18,20 +26,14 @@ public class ConvexHull {
 				m2--;
 			q1[m1++] = q2[m2++] = points[i];
 		}
-		List<Point> res = new ArrayList<>();
-		for (int i = 0; i < m1; i++)
-			res.add(q1[i]);
-		if (res.size() < n) {
-			int r = m2 - 2, l = 1;
-			if (inclusive) {
-				while (l <= r && q2[r].x == q2[m2 - 1].x && q2[r].y == q2[m2 - 1].y)
-					r--;
-				while (l <= r && q2[l].x == q2[0].x && q2[l].y == q2[0].y)
-					l++;
+		if (m1 < n) {
+			for (int i = m2 - 2; i >= 1; i--) {
+				if (!(q2[i].x == q2[0].x && q2[i].y == q2[0].y)
+						&& !(q2[i].x == q2[m2 - 1].x && q2[i].y == q2[m2 - 1].y)) {
+					q1[m1++] = q2[i];
+				}
 			}
-			for (int i = r; i >= l; i--)
-				res.add(q2[i]);
 		}
-		return res;
+		return Arrays.copyOf(q1, m1);
 	}
 }
